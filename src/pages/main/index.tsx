36fd806eac4899f7d58c4f88cedaa9629de97399
kangsinbeom <kanian16@naver.com>
2024-03-04 13:02:56 +0900

    feat: 사이드바 UI

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,39 @@
-import { Flex } from '@/components'
-import { Top } from './layouts'
+import { Circle, Flex, Header, ListRow, Sidebar } from '@/components'
+import { CATEGORY } from '@/constant'
+import getSortedColor from '@/utils/getSortedColor'
 
 const MainPage = () => {
   return (
     <Flex direction="column">
-      <Top />
+      <Header isSearch={true} />
+      <Flex>
+        <Sidebar
+          content={
+            <>
+              {CATEGORY.map(({ url, ko }, index) => {
+                const backgroundColor = getSortedColor({
+                  max: CATEGORY.length,
+                  color: 'blue',
+                })
+                console.log(backgroundColor[index])
+                return (
+                  <ListRow
+                    key={index}
+                    left={
+                      <Circle
+                        size={35}
+                        backgroundColor={backgroundColor[index]}
+                      />
+                    }
+                    contents={<ListRow.Category to={url} text={ko} />}
+                  />
+                )
+              })}
+            </>
+          }
+        />
+        <div>주요 컨텐츠</div>
+      </Flex>
     </Flex>
   )
 }
