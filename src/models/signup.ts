9d89b7bdd41b5e36a895c6756d2d86b00f9931ac
HyeonE <mag30102000@gmail.com>
2024-03-06 21:43:23 +0900

    feat: 기초 회원가입 기능 구현

---
+++ src/models/signup.ts
@@ -0,0 +1,6 @@
+export interface SignupFormValues {
+  email: string
+  nickname: string
+  password: string
+  rePassword: string
+}
--- src/pages/signup/components/SignupForm.tsx
+++ src/pages/signup/components/SignupForm.tsx
@@ -1,39 +1,111 @@
+import { getNicknameUser } from '@/apis/user'
 import { Button, Flex, TextField } from '@/components'
+import { SignupFormValues } from '@/models/signup'
 import { css } from '@emotion/react'
+import { useForm } from 'react-hook-form'
 
-const SignupForm = () => {
+const SignupForm = ({
+  onSubmit,
+}: {
+  onSubmit: (formValues: SignupFormValues) => void
+}) => {
+  const { register, formState, handleSubmit, getValues } =
+    useForm<SignupFormValues>({
+      mode: 'onChange',
+    })
+
+  const validateNickname = async () => {
+    try {
+      const users = await getNicknameUser(getValues().nickname)
+      return users.length === 0 || '이미 사용중인 닉네임입니다.'
+    } catch (error) {
+      return '닉네임 확인 중 오류가 발생했습니다.'
+    }
+  }
+
   return (
     <Flex direction="column" css={formContainerStyle}>
-      <Flex css={InputBoxStyle}>
-        <TextField placeholder="닉네임" name="nickname" />
-        <Button color="reverse">중복확인</Button>
-      </Flex>
-      <Flex css={InputBoxStyle}>
-        <TextField placeholder="이메일" name="email" />
-        <Button color="reverse">인증번호 전송</Button>
-      </Flex>
-      {/* <Flex css={InputBoxStyle}>
-          <Input placeholder="인증번호 입력" name="emailAuthNum" />
-          <Button color="reverse">확인</Button>
-          <Button color="reverse">재전송</Button>
-        </Flex> */}
-      <TextField placeholder="비밀번호" name="password" type="password" />
+      <TextField
+        placeholder="닉네임"
+        helpMessage={formState.errors['nickname']?.message as string}
+        hasError={formState.errors['nickname'] != null}
+        {...register('nickname', {
+          required: true,
+          pattern: VALIDATION_MESSAGE_MAP['nickname'],
+          validate: validateNickname,
+        })}
+      />
+      <TextField
+        placeholder="이메일"
+        helpMessage={formState.errors['email']?.message as string}
+        hasError={formState.errors['email'] != null}
+        {...register('email', {
+          required: true,
+          pattern: VALIDATION_MESSAGE_MAP['email'],
+        })}
+      >
+        {/* <Button color="reverse">인증번호 전송</Button> */}
+      </TextField>
+      {/* <TextField placeholder="인증번호 입력" name="emailAuthNum">
+        <Button color="reverse">확인</Button>
+        <Button color="reverse">재전송</Button>
+      </TextField> */}
+      <TextField
+        placeholder="비밀번호"
+        type="password"
+        helpMessage={formState.errors['password']?.message as string}
+        hasError={formState.errors['password'] != null}
+        {...register('password', {
+          required: true,
+          pattern: VALIDATION_MESSAGE_MAP['password'],
+        })}
+      />
       <TextField
         placeholder="비밀번호 확인"
-        name="repassword"
         type="password"
+        helpMessage={formState.errors['rePassword']?.message as string}
+        hasError={formState.errors['rePassword'] != null}
+        {...register('rePassword', {
+          required: true,
+          validate: {
+            matchPassword: (value) => {
+              return (
+                getValues().password === value || '비밀번호가 일치하지 않습니다'
+              )
+            },
+          },
+        })}
       />
-      <Button typography="t4">로그인</Button>
+      <Button typography="t4" onClick={handleSubmit(onSubmit)}>
+        회원가입
+      </Button>
     </Flex>
   )
 }
+
+const VALIDATION_MESSAGE_MAP: {
+  [key: string]: {
+    value: RegExp
+    message: string
+  }
+} = {
+  nickname: {
+    value: /^[가-힣a-zA-Z0-9]{2,10}$/,
+    message: '닉네임은 한글, 영어, 숫자 중 2글자 이상 10글자 이하입니다',
+  },
+  email: {
+    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    message: '이메일 형식을 확인해주세요',
+  },
+  password: {
+    value: /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9\S]{8,20}$/,
+    message: '비밀번호는 영어, 숫자가 섞여 8자 이상 20자 이하여야 합니다',
+  },
+}
+
 const formContainerStyle = css`
   width: 650px;
-  gap: 30px;
+  gap: 10px;
 `
 
-const InputBoxStyle = css`
-  gap: 10px;
-  width: 100%;
-`
 export default SignupForm
