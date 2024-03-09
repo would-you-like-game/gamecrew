76ef2d5afc799b209062f217b0a0e74a2a815410
HyeonE <mag30102000@gmail.com>
2024-03-09 22:44:08 +0900

    design: 폰트 bold 여부 추가

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,12 +16,14 @@ interface ButtonProps {
   full?: boolean
   disabled?: boolean
   typography?: Typography
+  bold?: boolean
 }
 const Button = styled.button<ButtonProps>(
   {
     cursor: 'pointer',
     fontWeight: 'bold',
     borderRadius: '10px',
+    bold: true,
   },
   ({ color = 'primary', weak }) =>
     weak ? buttonWeakMap[color] : buttonColorMap[color],
@@ -40,6 +42,14 @@ const Button = styled.button<ButtonProps>(
           cursor: initial;
         `
       : undefined,
+  ({ bold }) =>
+    bold
+      ? css`
+          font-weight: bold;
+        `
+      : css`
+          font-weight: normal;
+        `,
   ({ typography = 't5' }) => typographyMap[typography],
 )
 
