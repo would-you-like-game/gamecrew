cc21770c57dbdafbb1c7382a31c2e90ca9b5eec9
HyeonE <mag30102000@gmail.com>
2024-03-09 22:53:39 +0900

    design: 로그인 페이지 UI

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Flex, HeaderlessLayout, TextSpacing } from '@/components'
+import { GoogleButton, LoginForm } from './components'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <HeaderlessLayout text="로그인">
+      <Flex direction="column" justify="center" align="center" gap={40}>
+        <GoogleButton />
+        <TextSpacing position="center">이메일로 로그인</TextSpacing>
+        <LoginForm />
+        <TextSpacing position="bottom">
+          <Link to={'/signup'}>계정이 없으신가요? 간편가입하기</Link>
+        </TextSpacing>
+      </Flex>
+    </HeaderlessLayout>
   )
 }
 
