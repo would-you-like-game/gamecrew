9a5c1acc229eca0684aba0cad7fc8179935cdbc0
HyeonE <mag30102000@gmail.com>
2024-03-11 23:25:20 +0900

    feat: 유효성 검사 규칙 분리 닉네임, 이메일, 비밀번호

--- src/pages/signup/components/SignupForm.tsx
+++ src/pages/signup/components/SignupForm.tsx
@@ -1,6 +1,7 @@
 import { getNicknameUser } from '@/apis/user'
 import { Button, Flex, TextField } from '@/components'
 import { SignupFormValues } from '@/models/signup'
+import VALIDATION_MESSAGE_MAP from '@/utils/validation'
 import { css } from '@emotion/react'
 import { useForm } from 'react-hook-form'
 
@@ -83,25 +84,6 @@ const SignupForm = ({
   )
 }
 
-const VALIDATION_MESSAGE_MAP: {
-  [key: string]: {
-    value: RegExp
-    message: string
-  }
-} = {
-  nickname: {
-    value: /^[가-힣a-zA-Z0-9]{2,10}$/,
-    message: '닉네임은 한글, 영어, 숫자 중 2글자 이상 10글자 이하입니다',
-  },
-  email: {
-    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    message: '이메일 형식을 확인해주세요',
-  },
-  password: {
-    value: /^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9\S]{8,20}$/,
-    message: '비밀번호는 영어, 숫자가 섞여 8자 이상 20자 이하여야 합니다',
-  },
-}
 
 const formContainerStyle = css`
   width: 650px;
